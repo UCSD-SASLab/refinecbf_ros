2529a67d208c9d721bc5e847854394655d5e2f5c
Sander Tonkens <43548774+stonkens@users.noreply.github.com>
2024-02-01 15:31:27 -0800

    Vf passing option (#14)
    * Implemented Initiating with CBF
    
    * Debugging non-updating CBF
    
    * Debugging Assertion Error
    
    * Working updates with sdf, vf, cbf
    
    * Incorporated Ethan's changes
    
    ---------
    
    Co-authored-by: skojima798 <skojima@ucsd.edu>
    Co-authored-by: MatthewK0617 <kimmatthew2022@gmail.com>

--- refinecbf_ros/scripts/refine_cbf_node.py
+++ refinecbf_ros/scripts/refine_cbf_node.py
@@ -4,6 +4,7 @@ import rospy
 import numpy as np
 import jax.numpy as jnp
 from refinecbf_ros.msg import ValueFunctionMsg, Array, HiLoArray
+from std_msgs.msg import Bool
 from cbf_opt import ControlAffineASIF
 from refine_cbfs import TabularControlAffineCBF
 from refinecbf_ros.config import Config
@@ -17,8 +18,14 @@ class SafetyFilterNode:
 
     def __init__(self):
         self.safety_filter_active = rospy.get_param("~safety_filter_active", True)
-        vf_topic = rospy.get_param("~topics/vf_update", "/vf_update")
-        self.vf_sub = rospy.Subscriber(vf_topic, ValueFunctionMsg, self.callback_vf_update)
+        vf_topic = rospy.get_param("~topics/vf_update")
+        self.vf_update_method = rospy.get_param("~vf_update_method")
+        if self.vf_update_method == "pubsub":
+            self.vf_sub = rospy.Subscriber(vf_topic, ValueFunctionMsg, self.callback_vf_update_pubsub)
+        elif self.vf_update_method == "file":
+            self.vf_sub = rospy.Subscriber(vf_topic, Bool, self.callback_vf_update_file)
+        else:
+            raise NotImplementedError("{} is not a valid vf update method".format(self.vf_update_method))
         self.state_topic = rospy.get_param("~topics/state", "/state_array")
         self.state_sub = rospy.Subscriber(self.state_topic, Array, self.callback_state)
 
@@ -55,7 +62,16 @@ class SafetyFilterNode:
         self.safety_filter_solver.umin = np.array(msg.lo)
         self.safety_filter_solver.umax = np.array(msg.hi)
 
-    def callback_vf_update(self, vf_msg):
+    def callback_vf_update_file(self, vf_msg):
+        if not vf_msg.data:
+            return
+        self.cbf.vf_table = np.array(np.load('./vf.npy')).reshape(self.grid.shape)
+        print("Updated vf")
+        if not self.initialized_safety_filter:
+            rospy.loginfo("Initialized safety filter")
+            self.initialized_safety_filter = True
+
+    def callback_vf_update_pubsub(self, vf_msg):
         self.cbf.vf_table = np.array(vf_msg.vf).reshape(self.grid.shape)
         print("Updated vf")
         if not self.initialized_safety_filter:
