9a065b8b47b658c0ca7973806f83f500d16668ff
Judy021 <judy.mohamad021@gmail.com>
2024-02-15 10:15:57 +0900

    Added actuation_update to modify environment node

--- refinecbf_ros/scripts/modify_environment.py
+++ refinecbf_ros/scripts/modify_environment.py
@@ -12,18 +12,20 @@ class ModifyEnvironmentServer:
         
         # Load configuration
         config = Config(hj_setup=True)
-
         self.disturbance_space = config.disturbance_space
+        self.control_space = config.control_space
 
+        # Set up publishers
         actuation_update_topic = rospy.get_param("~topics/actuation_update")
         self.actuation_update_pub = rospy.Publisher(actuation_update_topic, HiLoArray, queue_size=1)
 
         disturbance_update_topic = rospy.get_param("~topics/disturbance_update")
         self.disturbance_update_pub = rospy.Publisher(disturbance_update_topic, HiLoArray, queue_size=1)
 
-        # Set up service
+        # Set up services
         modify_environment_service = rospy.get_param("~services/modify_environment")
         rospy.Service(modify_environment_service, ModifyEnvironment, self.handle_modified_environment)
+
 
     def update_disturbances(self):
         hi = np.array(self.disturbance_space["hi"])
@@ -31,20 +33,28 @@ class ModifyEnvironmentServer:
         # self.disturbance_update_pub.publish(Bool(True))
         self.disturbance_update_pub.publish(HiLoArray(hi=hi, lo=lo))
     
+    def update_actuation(self):
+        hi = np.array(self.control_space["hi"])
+        lo = np.array(self.control_space["lo"])
+        # self.disturbance_update_pub.publish(Bool(True))
+        self.actuation_update_pub.publish(HiLoArray(hi=hi, lo=lo))
+    
     def handle_modified_environment(self, req):
         '''
         To add disturbances, paste the following in a terminal:
-          rosservice call /env/modify_environment "disturbance"
+          rosservice call /env/modify_environment "update_disturbance"
+          rosservice call /env/modify_environment "update_actuation"
         '''
         modification_request = req.modification
         # if modification_request == "actuation":
         #     # Get the new actuation values
         #     self.actuation_update_pub.publish()
-        if modification_request == "disturbance":
-            rospy.loginfo("Start")
+        if modification_request == "update_disturbance":
             self.update_disturbances()
-            rospy.loginfo("Stop")
-            output = "Disturbance Activated"
+            output = "Disturbance Updated"
+        elif modification_request == "update_actuation":
+            self.update_actuation()
+            output = "Actuation Updated"
         return ModifyEnvironmentResponse(output)
 
 
